b43da0cd3f11bcd117cd665367d5061790ac0fc5
deepak-quovantis <deepak.prasad@quovantis.com>
2021-11-02 16:58:13 +0530

    multiple chat rooms

--- index.js
+++ index.js
@@ -10,13 +10,23 @@ server.listen(port,()=>{
 app.get('/',(req,res) => {
     res.sendFile(__dirname+ '/public/index.html')
 });
+app.get('/javascript',(req,res) => {
+    res.sendFile(__dirname+ '/public/javascript.html')
+});
+app.get('/angular',(req,res) => {
+    res.sendFile(__dirname+ '/public/angular.html')
+});
+app.get('/aws',(req,res) => {
+    res.sendFile(__dirname+ '/public/aws.html')
+});
 
 io.on('connection', (socket) =>{
-    console.log('user Connected');
-    console.log(socket);
-    socket.on('message', (msg) => {
-        console.log(msg)
-        io.emit('message', msg)
+    socket.on('join',(data)=>{
+        socket.join(data.room);
+        io.in(data.room).emit('message',`New user connected ${data.room} room!`)
+    })
+    socket.on('message', (data) => {
+        io.in(data.room).emit('message', data.msg)
     });
     
 })
